0d6eeeb301b4388866079d1726436bd182a1ba42
yudhaeric <yudhaericpamungkas@gmail.com>
2026-05-21 09:37:51 +0700

    feature(SV-Articles): Fix Issue CORS

--- main.go
+++ main.go
@@ -8,19 +8,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// Initialize Database
-	config.ConnectDatabase()
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-	// Auto Migration
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func main() {
+	config.ConnectDatabase()
 	config.DB.AutoMigrate(&models.Article{})
 
-	// Initialize Router
 	r := gin.Default()
-
-	// Setup Routes
+	r.Use(CORSMiddleware())
 	routes.SetupRoutes(r)
-
-	// Run Server
 	r.Run(":8080")
 }
